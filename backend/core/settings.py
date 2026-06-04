599c2764321d7c1f47c304eff57f91b82f1a3515
Carlos Gonzalez <carlosg@MacBook-Air-de-Carlos-2.local>
2026-06-04 16:50:57 -0600

    configuracion de base de datos postgresql

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
